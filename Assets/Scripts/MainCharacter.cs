326ff6640c1b21591d9b7ca6af7a0909d56392c3
Evildavo <evildavo@gmail.com>
2017-04-18 11:09:00 +1000

    Player can walk left or right

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-// Main player character.
+// Main player character. Requires an animator and a rigidbody.
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class MainCharacter : MonoBehaviour
 {
     Animator animator;
+    Rigidbody2D ourRigidBody;
+    Vector2 movementForce = new Vector2(0f, 0f);
+
+    [SerializeField]
+    [Tooltip("Speed the character walks left or right")]
+    float walkSpeed = 1f;
 
 	void Start ()
     {
         animator = GetComponent<Animator>();
-	}
+        ourRigidBody = GetComponent<Rigidbody2D>();
+    }
 	
 	void Update ()
     {
@@ -22,6 +31,8 @@ public class MainCharacter : MonoBehaviour
         {
             animator.SetBool("IsFacingLeft", false);
             animator.SetBool("IsWalking", true);
+            movementForce.Set(walkSpeed, 0f);
+            ourRigidBody.AddForce(movementForce);
         }
 
         // Walk left.
@@ -29,12 +40,15 @@ public class MainCharacter : MonoBehaviour
         {
             animator.SetBool("IsFacingLeft", true);
             animator.SetBool("IsWalking", true);
+            movementForce.Set(-walkSpeed, 0f);
+            ourRigidBody.AddForce(movementForce);
         }
 
         // Stand
         else
         {
             animator.SetBool("IsWalking", false);
+            //ourRigidBody.velocity = Vector2.zero; // Stop abruptly.
         }
     }
 
