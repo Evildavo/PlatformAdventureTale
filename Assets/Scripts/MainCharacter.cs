54453f47e35d17a6186f01430e540c790d2ca6c8
Evildavo <evildavo@gmail.com>
2017-04-18 10:26:15 +1000

    Player shows left and right walking animation on key input

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Main player character.
+public class MainCharacter : MonoBehaviour
+{
+    Animator animator;
+
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Walk right.
+        if (horizontalInput > 0f)
+        {
+            animator.SetBool("IsFacingLeft", false);
+            animator.SetBool("IsWalking", true);
+        }
+
+        // Walk left.
+        else if (horizontalInput < 0f)
+        {
+            animator.SetBool("IsFacingLeft", true);
+            animator.SetBool("IsWalking", true);
+        }
+
+        // Stand
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+
+}
